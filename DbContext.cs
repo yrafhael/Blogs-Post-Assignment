a1569c15752b6fbcbb4f5de0a9ec988ca26131ba
Yose Rafhael <yrafhael@my.wctc.edu>
2024-04-18 06:27:23 -0500

    [1.01] Initial commit - commit all files that has been done in lab activity - all connection string password are removed from files

---
+++ DbContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+    public void AddBlog(Blog blog)
+    {
+        this.Blogs.Add(blog);
+        this.SaveChanges();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+                var configuration =  new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.json");
+
+        var config = configuration.Build();
+        optionsBuilder.UseSqlServer(@config["BlogsConsole:ConnectionString"]);
+    }
+}
